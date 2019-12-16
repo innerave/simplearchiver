a1ca2f9af96239cebc1b6819e4a28ba765d90222
Alexander Vladimirov <innerave@gmail.com>
2019-12-16 22:37:27 +0300

    Добавлена функция создания директорий

--- src/archiver.c
+++ src/archiver.c
@@ -1,2 +1,40 @@
 #include "archiver.h"
 
+
+// рекурсивное создание директорий
+int r_mkdir(const char * dir){
+    int return_code = 0;
+    const size_t len = strlen(dir);
+
+    if (!len){
+        return 0;
+    }
+
+    char * path = calloc(len + 1, sizeof(char));
+    strncpy(path, dir, len);
+
+    // удаление '/'
+    if (path[len - 1] ==  '/'){
+        path[len - 1] = 0;
+    }
+
+    // все последующие каталоги не существуют
+    for(char * p = path + 1; *p; p++){
+        if (*p == '/'){
+            *p = '\0';
+
+            if ((return_code = mkdir(path, DIR_MODE))){
+                fprintf("Невозможно создать директорию %s: %s", path, strerror(return_code));
+            }
+
+            *p = '/';
+        }
+    }
+
+    if (mkdir(path, DIR_MODE) < 0){
+        fprintf("Невозможно создать директорию %s: %s", path, strerror(return_code));
+    }
+
+    free(path);
+    return 0;
+}
--- src/archiver.h
+++ src/archiver.h
@@ -2,3 +2,11 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIR_MODE S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
+
+// рекурсивное создание директорий
+int r_mkdir(const char * dir);
