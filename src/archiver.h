50c4e18aa1b217fee11a45f301a2faf67cb062ac
Alexander Vladimirov <innerave@gmail.com>
2019-12-16 22:25:52 +0300

    Убран говнокод глеба

--- src/archiver.h
+++ src/archiver.h
@@ -2,26 +2,3 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <string.h>
-//обработка одного файла 
-//(добавляем размер и название в pre_info.meta 
-// а данные файла в pre_arch)
-int add_file_meta(char *path);
-//создание архива(хз поч так назвал, переиминуйте)
-int make_meta(char *name,char **argv,int optind,const int argc);
-//добавление метаинформации и тела архива в конечный файл
-int make_arch(char *name);
-//добавляем в info.meta количество файлов (sizeof(int),1)
-//добавляем метаинформацию в info.meta
-int add_files_count(int files);
-//копируем побитово из одного файлового потока в другой
-//возвращаем количество скопированных битов
-long int copy_open_file(FILE *from,FILE *to);
-//то же что и выше, только n битов
-long int copy_open_file_by_n(FILE *from,FILE *to,long int n);
-/*надо изменить названия
-	создаем info.meta с метаинформацией 
-	и пофайлово сохраняем из архива*/
-int take_meta(char *name);
-/*сохранить файл из arch с именем и размером из info
-по пути path (пока path приписывает вначале имени файла строку)*/
-int save_file(char *path,FILE *arch,FILE *info);
--- src/main.c
+++ src/main.c
@@ -3,16 +3,17 @@
 int main(int argc, char *argv[])
 {
     int opt;
-
+    /*  optarg – указатель на текущий аргумент, если таковой имеется.
+        optind – индекс на следующий указатель argv, который будет обработан при следующем вызове getopt().
+        optopt – нераспознанная опция.
+        argv[optind] – и далее – собранные опции*/
     while((opt = getopt(argc, argv, ":e:c:")) != -1) {
         switch(opt) {
         case 'e':
             printf("Извлечение из архива : %s\n", optarg);
-            take_meta(optarg);
             break;
         case 'c':
             printf("Добавление в архив: %s\n", optarg);
-            make_meta(optarg,argv,optind,argc);
             break;
         case ':':
             printf("Требуется имя архива\n");
