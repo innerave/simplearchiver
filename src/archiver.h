a62a2c168cdbb7323aa338cf8cfea56f808c4140
Alexander Vladimirov <innerave@gmail.com>
2019-12-16 23:17:03 +0300

    Добавлен вывод выбранных файлов

--- src/archiver.h
+++ src/archiver.h
@@ -7,6 +7,10 @@
 #include <stdlib.h>
 
 #define DIR_MODE S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
-
+/*	файловые биты используются для определения типа файла.
+	S_IRWXU чтение, запись, выполнение / поиск владельцем
+	S_IRGRP разрешение на чтение
+	S_IXGRP разрешение на выполнение / поиск 
+*/
 // рекурсивное создание директорий
 int r_mkdir(const char * dir);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     /*  optarg – указатель на текущий аргумент, если таковой имеется.
         optind – индекс на следующий указатель argv, который будет обработан при следующем вызове getopt().
         optopt – нераспознанная опция.
-        argv[optind] – и далее – собранные опции*/
+        argv[optind] – и далее – собранные файлы*/
     while((opt = getopt(argc, argv, ":e:c:")) != -1) {
         switch(opt) {
         case 'e':
@@ -22,6 +22,8 @@ int main(int argc, char *argv[])
             printf("Неизвестная команда: %c\n", optopt);
             break;
         }
+        for(; optind < argc; optind++)
+        printf("Выбранные файлы: %s\n", argv[optind]);
     }
     return 0;
 }
